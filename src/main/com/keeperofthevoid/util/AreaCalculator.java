21c09df4641674909d170ccd531e036fc585196d
l640395 <lalmansingh.matthew@heb.com>
2016-07-21 10:00:09 -0500

    FEAT: master Removed commented out code to better show examples.

--- src/main/com/keeperofthevoid/util/AreaCalculator.java
+++ src/main/com/keeperofthevoid/util/AreaCalculator.java
@@ -1,7 +1,5 @@
 package com.keeperofthevoid.util;
 
-import com.keeperofthevoid.model.Shape;
-
 /**
  * AreaCalculator
  *
@@ -17,9 +15,6 @@ public class AreaCalculator {
      * @return Area for square (Width: 2, Height: 2)
      */
     public Integer calculateAreaForSquare() {
-        /*Shape square = new Shape(2,2);
-        // Area is 4
-        return square.getWidth() * square.getHeight();*/
         return null;
     }
 
@@ -28,9 +23,6 @@ public class AreaCalculator {
      * @return Area of triangle (Width: 2, Height: 4)
      */
     public Double calculateAreaForTriangle() {
-        /*Shape triangle = new Shape(2,4);
-        // Area is 4
-        return (triangle.getWidth() * triangle.getHeight())/2.0;*/
         return null;
     }
 
@@ -39,9 +31,6 @@ public class AreaCalculator {
      * @return Area of circle (Radius: 2)
      */
     public Double calculateAreaForCircle() {
-        /*Shape circle = new Shape(2);
-        // Area is 12.566
-        return Math.PI * Math.pow((double)circle.getWidth(), 2);*/
         return null;
     }
 }
