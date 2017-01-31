c2eeceb8de86362b073ea5e394f3c267f9aa8c76
l640395 <lalmansingh.matthew@heb.com>
2017-01-31 11:38:27 -0600

    FEAT: example-one-refactor Added base class and interface for die

---
+++ src/main/java/com/heb/unit/model/Die.java
@@ -0,0 +1,10 @@
+package com.heb.unit.model;
+
+/**
+ * Die Interface
+ *
+ * @author l640395 Matthew Lalmansingh
+ * @since 1/31/2017
+ */
+public interface Die {
+}
---
+++ src/main/java/com/heb/unit/model/impl/DieImpl.java
@@ -0,0 +1,12 @@
+package com.heb.unit.model.impl;
+
+import com.heb.unit.model.Die;
+
+/**
+ * Die Implementation Class
+ *
+ * @author l640395 Matthew Lalmansingh
+ * @since 1/31/2017
+ */
+public class DieImpl implements Die {
+}
