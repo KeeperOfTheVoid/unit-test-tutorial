f0ae00ca55b3536e1a44c90d3a792ee4f72d6870
l640395 <lalmansingh.matthew@heb.com>
2017-03-06 16:56:40 -0600

    FEAT: master Cleared all known solutions for tests.  Merged in example one because I didn't want to deal with creating the files again.

--- src/main/java/com/heb/unit/util/AreaCalculator.java
+++ src/main/java/com/heb/unit/util/AreaCalculator.java
@@ -1,7 +1,5 @@
 package com.heb.unit.util;
 
-import com.heb.unit.model.Shape;
-
 /**
  * AreaCalculator
  *
@@ -12,73 +10,15 @@ import com.heb.unit.model.Shape;
  * @since 5/20/16
  */
 public class AreaCalculator {
-    /**
-     * Calculates area of square
-     * @return Area for square (Width: 2, Height: 2)
-     */
     public Integer calculateAreaForSquare() {
-        Shape square = new Shape(2,2);
-        // Area is 4
-        return square.getWidth() * square.getHeight();
+        return null;
     }
 
-    /**
-     * Calculates area of square
-     * @param width Width of square
-     * @param height Height of square
-     * @return Area for square (Width: 2, Height: 2)
-     */
-    public Integer calculateAreaForSquare(Integer width, Integer height) {
-        Shape square = new Shape();
-        square.setWidth(width);
-        square.setHeight(height);
-        // Area is 4
-        return square.getWidth() * square.getHeight();
-    }
-
-    /**
-     * Calculates area of triangle
-     * @return Area of triangle (Width: 2, Height: 4)
-     */
     public Double calculateAreaForTriangle() {
-        Shape triangle = new Shape(2,4);
-        // Area is 4
-        return (triangle.getWidth() * triangle.getHeight())/2.0;
+        return null;
     }
 
-    /**
-     * Calculates area of triangle
-     * @param width Width of triangle
-     * @param height Height of triangle
-     * @return Area of triangle (Width: 2, Height: 4)
-     */
-    public Double calculateAreaForTriangle(Integer width, Integer height) {
-        Shape triangle = new Shape();
-        triangle.setWidth(width);
-        triangle.setHeight(height);
-        // Area is 4
-        return (triangle.getWidth() * triangle.getHeight())/2.0;
-    }
-
-    /**
-     * Calculates area of circle
-     * @return Area of circle (Radius: 2)
-     */
     public Double calculateAreaForCircle() {
-        Shape circle = new Shape(2);
-        // Area is 12.566
-        return Math.PI * Math.pow((double)circle.getWidth(), 2);
-    }
-
-    /**
-     * Calculates area of circle
-     * @param width Radius of circle
-     * @return Area of circle (Radius: 2)
-     */
-    public Double calculateAreaForCircle(Integer width) {
-        Shape circle = new Shape();
-        circle.setWidth(width);
-        // Area is 12.566
-        return Math.PI * Math.pow((double)circle.getWidth(), 2);
+        return null;
     }
 }
